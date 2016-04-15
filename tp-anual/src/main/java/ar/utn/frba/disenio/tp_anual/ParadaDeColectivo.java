adbe3f4e7f2176f34157d43ac360582a432d8013
Il Mostro <franco.balduzzi@hotmail.com>
2016-04-14 23:14:40 -0300

    agrego implementacion de linea de colectivo

--- tp-anual/src/main/java/ar/utn/frba/disenio/tp_anual/ParadaDeColectivo.java
+++ tp-anual/src/main/java/ar/utn/frba/disenio/tp_anual/ParadaDeColectivo.java
@@ -3,7 +3,10 @@ package ar.utn.frba.disenio.tp_anual;
 import java.sql.Date;
 
 public class ParadaDeColectivo extends POI{
-
+	public ParadaDeColectivo(String linea)
+	{
+		this.addTag(linea);
+	}
 	public Boolean esCercano(Coordenada coordenada) {
 		return (distancia(this.getCoordenada(), coordenada) < 100);		
 	}
