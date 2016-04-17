26c6d06f88de1125ff7b2a100148ffb9b064f49f
martinvergara26 <martin.vergara@tictapps.com>
2016-04-17 13:41:50 -0300

    Agregada validación de horario bancario

--- tp-anual/src/main/java/ar/utn/frba/disenio/tp_anual/SucursalBanco.java
+++ tp-anual/src/main/java/ar/utn/frba/disenio/tp_anual/SucursalBanco.java
@@ -10,6 +10,8 @@ import org.uqbar.geodds.Polygon;
 public class SucursalBanco extends POI{
 
 	private List<Servicio> servicios;
+	private static Integer horarioBancarioArranque = 10;
+	private static Integer horarioBancarioCierre = 15;
 	
 	public void addServicio(Servicio servicio)
 	{
@@ -23,11 +25,18 @@ public class SucursalBanco extends POI{
 
 	public Boolean estaDisponible(LocalDateTime fecha, String valorX) 
 	{
+		if(fueraDeHorarioBancario(fecha)) return false;
 		return servicios.stream().anyMatch(servicio -> servicio.toString().equals(valorX) && servicio.estaDisponibe(fecha));
 	}
 	
+	private boolean fueraDeHorarioBancario(LocalDateTime fecha) {
+		return (fecha.getHour()<horarioBancarioArranque ||
+				fecha.getHour()>horarioBancarioCierre);
+	}
+
 	public Boolean estaDisponible(LocalDateTime fecha)
 	{
+		if(fueraDeHorarioBancario(fecha)) return false;
 		return servicios.stream().anyMatch(servicio -> servicio.estaDisponibe(fecha));
 	}
 	
