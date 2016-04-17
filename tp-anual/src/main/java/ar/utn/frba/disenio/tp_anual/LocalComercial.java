91996cf0d8cb1991e8861c5c0977fa83461b24cb
martinvergara26 <martin.vergara@tictapps.com>
2016-04-17 14:13:06 -0300

    A LocalComercial le pongo una lista de disponibilidades

--- tp-anual/src/main/java/ar/utn/frba/disenio/tp_anual/LocalComercial.java
+++ tp-anual/src/main/java/ar/utn/frba/disenio/tp_anual/LocalComercial.java
@@ -3,32 +3,37 @@ package ar.utn.frba.disenio.tp_anual;
 import java.sql.Date;
 import java.time.DayOfWeek;
 import java.time.LocalDateTime;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class LocalComercial extends POI{
 
 	private Integer radioDeCercania;
-	private int horarioArranque;
-	private int horarioCierre;
-	private List<DayOfWeek> diasDeAtencion;
-	public void addDiaLaboral(DayOfWeek dia)
-	{
-		diasDeAtencion.add(dia);
+	private Set<Disponibilidad> disponibilidades;
+	
+	public void addDisponibilidad(Disponibilidad disponibilidad){
+		disponibilidades.add(disponibilidad);
 	}
-	public void deleteDiaLaboral(DayOfWeek dia)
-	{
-		diasDeAtencion.remove(dia);
-	}
+	
+	//TODO removeDisponibilidad
+	
 	public LocalComercial(int radio)
 	{
 		radioDeCercania = new Integer(radio);
+		disponibilidades = new HashSet<>();
 	}
 	public Boolean esCercano(Coordenada coordenada) {
 		return (distancia(this.getCoordenada(), coordenada) < this.radioDeCercania.intValue());
 	}
 
+	public Boolean estaDisponible(LocalDateTime fecha) {
+		return disponibilidades.stream().anyMatch(disponibilidad -> disponibilidad.estaDisponible(fecha));
+	}
+	
+	@Override
 	public Boolean estaDisponible(LocalDateTime fecha, String valorX) {
-		return (diasDeAtencion.contains(fecha.getDayOfWeek()) && fecha.getHour() > horarioArranque && fecha.getHour()< horarioCierre);
+		return estaDisponible(fecha);
 	}
 
 }
