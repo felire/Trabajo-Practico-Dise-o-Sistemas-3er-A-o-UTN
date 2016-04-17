b65c37685e8c67f3bd3e76d77b10874dd5401288
martinvergara26 <martin.vergara@tictapps.com>
2016-04-16 23:54:22 -0300

    Refactor de estaDisponible

--- tp-anual/src/main/java/ar/utn/frba/disenio/tp_anual/SucursalBanco.java
+++ tp-anual/src/main/java/ar/utn/frba/disenio/tp_anual/SucursalBanco.java
@@ -24,8 +24,13 @@ public class SucursalBanco extends POI{
 	public Boolean estaDisponible(LocalDateTime fecha, String valorX) 
 	{
 		if(isEmpty(valorX)) return true;
-		return servicios.stream().anyMatch(servicio -> servicio.toString().equals(valorX) && servicio.estaDisponibe(fecha));
+		return servicios.stream().anyMatch(servicio -> cumpleDisponibilidad(fecha, valorX, servicio));
 	}
+	
+	private boolean cumpleDisponibilidad(LocalDateTime fecha, String valorX, Servicio servicio) {
+		return servicio.toString().equals(valorX) && servicio.estaDisponibe(fecha);
+	}
+	
 	public Boolean estaDisponibel(LocalDateTime fecha)
 	{
 		return servicios.stream().anyMatch(servicio -> servicio.estaDisponibe(fecha));
