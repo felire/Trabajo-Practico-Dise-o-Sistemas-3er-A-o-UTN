1fd767628047bafbf592719be8150f21935f2211
martinvergara26 <martin.vergara@tictapps.com>
2016-04-17 16:24:50 -0300

    Le pongo Disponibilidad a Servicio

--- tp-anual/src/main/java/ar/utn/frba/disenio/tp_anual/Servicio.java
+++ tp-anual/src/main/java/ar/utn/frba/disenio/tp_anual/Servicio.java
@@ -4,22 +4,13 @@ import java.time.DayOfWeek;
 import java.time.LocalDateTime;
 import java.util.List;
 
-public class Servicio {//Atencion, para modelar esto podriamos hacer que cada dia maneje sus horarios ya que algunos negocios son asi que se y.
+public class Servicio {
 	private String nombre;
-	private int horarioArranque;
-	private int horarioCierre;
-	private List<DayOfWeek> diasDeAtencion;
-	public void addDiaLaboral(DayOfWeek dia)
-	{
-		diasDeAtencion.add(dia);
-	}
-	public void deleteDiaLaboral(DayOfWeek dia)
-	{
-		diasDeAtencion.remove(dia);
-	}
+	private List<Disponibilidad> diasDisponibles;
+	
 	public boolean estaDisponibe(LocalDateTime fecha)
 	{
-		return (diasDeAtencion.contains(fecha.getDayOfWeek()) && fecha.getHour() > horarioArranque && fecha.getHour()< horarioCierre);
+		return diasDisponibles.stream().anyMatch(disponibilidad -> disponibilidad.estaDisponible(fecha));
 	}
 	
 	public String toString()
