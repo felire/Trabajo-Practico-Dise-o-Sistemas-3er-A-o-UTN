893c7e122662e09f3f1d24ea622d0bac550209da
Felire <feliress13@gmail.com>
2016-04-25 14:52:57 -0300

    Subo una cosita no esta terminado

--- tp-anual/src/main/java/ar/utn/frba/disenio/tp_anual/DisponibilidadHoraria.java
+++ tp-anual/src/main/java/ar/utn/frba/disenio/tp_anual/DisponibilidadHoraria.java
@@ -2,17 +2,24 @@ package ar.utn.frba.disenio.tp_anual;
 
 import java.time.DayOfWeek;
 import java.time.LocalDateTime;
-import java.time.LocalTime;
 import java.util.ArrayList;
+import java.util.List;
 
 public class DisponibilidadHoraria {
-	private ArrayList<DiaLaboral> disponibilidades;
+	private List<FranjaHoraria> franjas;
+	private List<DayOfWeek> dias;
 	
-	public DisponibilidadHoraria(ArrayList<DiaLaboral> disponibilidades){
-		this.disponibilidades=disponibilidades;		
+	public DisponibilidadHoraria(ArrayList<FranjaHoraria> franjas, ArrayList<DayOfWeek>  dias){
+		this.franjas = franjas;
+		this.dias = dias;		
 	}
-
+	public Boolean diaDisponible(LocalDateTime fecha){
+		return this.dias.contains(fecha.getDayOfWeek());
+	}
+	public Boolean horaDisponible(LocalDateTime fecha){
+		return this.franjas.stream().anyMatch(franja->franja.contiene(fecha.toLocalTime()));
+	}
 	public Boolean estaDisponible(LocalDateTime fecha){
-		return disponibilidades.stream().anyMatch(disponibilidad -> (disponibilidad.estaDisponible(fecha)));
+		return diaDisponible(fecha) && horaDisponible(fecha);
 	}
 }
