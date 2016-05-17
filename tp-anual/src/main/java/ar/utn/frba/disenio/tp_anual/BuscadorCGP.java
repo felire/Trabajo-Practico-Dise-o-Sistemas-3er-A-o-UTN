6f1d7f7c56fee6de25471eb5714052141418b6ba
felire <feliress13@gmail.com>
2016-05-17 16:53:44 -0300

    Subo Buscador CGP terminado con un test para el mismo

--- tp-anual/src/main/java/ar/utn/frba/disenio/tp_anual/BuscadorCGP.java
+++ tp-anual/src/main/java/ar/utn/frba/disenio/tp_anual/BuscadorCGP.java
@@ -10,15 +10,18 @@ public class BuscadorCGP{
 	public List<POI> filtrarCGPs(String palabraClave){//Hardcodeado hasta tener correcto funcionamiento
 		List<CentroDTO> listaDevolucion = servicioExterno.search(palabraClave);
 		if(listaDevolucion != null){
-			
+			return this.crearCGPs(listaDevolucion);
 		}
 		return new ArrayList<POI>();
 	}
-	public List<CGP> crearCGPs(List<CentroDTO> lista){
-		List<CGP> listaCGPS = new ArrayList<CGP>();
+	public List<POI> crearCGPs(List<CentroDTO> lista){
+		List<POI> listaCGPS = new ArrayList<POI>();
 		for(int i = 0; i< lista.size(); i++){
 			listaCGPS.add(adapterCGP.traducir(lista.get(i)));
 		}
 		return listaCGPS;
+	}
+	public void setAdapterCGP(AdapterCGP adapterCGP) {
+		this.adapterCGP = adapterCGP;
 	}
 }
---
+++ tp-anual/src/test/java/ar/utn/frba/disenio/tp_anual/TestBuscadorCGP.java
@@ -0,0 +1,77 @@
+package ar.utn.frba.disenio.tp_anual;
+
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.uqbar.geodds.Point;
+import org.uqbar.geodds.Polygon;
+
+public class TestBuscadorCGP {
+
+	AdapterCGP adapterCGP;
+	CentroDTO centroDTO;
+	ServicioDTO servicioDTOBicis;
+	RangoServicioDTO rangoServicioBicis;
+	CGP POITraducido;
+	LocalDate localDate;
+	LocalDateTime fecha;
+	BuscadorCGP buscadorCGP = new BuscadorCGP();
+	List<CentroDTO> listaCentroDTO = new ArrayList<CentroDTO>();
+	List<POI> listaCGP;
+	@Before
+	public void setUp(){
+		centroDTO=new CentroDTO();
+		//Hago el poligono de la comuna
+		Polygon polygon = new Polygon();
+		Point punto1 = new Point(0,0);
+		Point punto2 = new Point(0,100);
+		Point punto3 = new Point(100,100);
+		Point punto4 = new Point(100,0);
+		polygon.add(punto1);
+		polygon.add(punto2);
+		polygon.add(punto3);
+		polygon.add(punto4);
+		//Seteo Primer servicio
+		servicioDTOBicis= new ServicioDTO();
+		servicioDTOBicis.setNombre("Alquiler Bicicletas"); 
+		
+		//Su rango de atencion
+		rangoServicioBicis=new RangoServicioDTO();
+		rangoServicioBicis.setDia(6);
+		rangoServicioBicis.setHorarioDesde(9);
+		rangoServicioBicis.setMinutoDesde(0);
+		rangoServicioBicis.setHorarioHasta(17);
+		rangoServicioBicis.setMinutoHasta(30);
+		servicioDTOBicis.agregarRango(rangoServicioBicis);
+		
+		//Ahora si, seteo atributos centro DTO
+		centroDTO.setComuna(polygon);
+		centroDTO.setDomicilio("Arana 1551");
+		centroDTO.setNombreDirector("Tito Lasanta");
+		centroDTO.setTelefono("45318008");
+		centroDTO.setZonasIncluidas("Balvanera, Villa Ortuza");
+		centroDTO.agregarServicioDTO(servicioDTOBicis);
+		
+		//Creo el adapter
+		adapterCGP= new AdapterCGP();
+		
+		//Creo una fecha para probar disponibilidad
+		localDate = LocalDate.of(2016, 5, 28);
+		fecha = localDate.atTime(14, 30);
+		listaCentroDTO.add(centroDTO);
+		buscadorCGP.setAdapterCGP(adapterCGP);
+	}
+	
+	@Test
+	public void TestArmarListaCGP(){
+		listaCGP = buscadorCGP.crearCGPs(listaCentroDTO);
+		assertEquals("Nuevo CGP",listaCGP.get(0).getNombre());
+	}
+
+}
