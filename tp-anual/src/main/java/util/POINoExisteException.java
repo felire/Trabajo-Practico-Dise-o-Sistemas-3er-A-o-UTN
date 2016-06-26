a104253b6b92690d5b77a1422edf854120f1276e
martinvergara26 <martin.vergara@tictapps.com>
2016-06-26 19:15:46 -0300

    Override de getMessage

--- tp-anual/src/main/java/util/POINoExisteException.java
+++ tp-anual/src/main/java/util/POINoExisteException.java
@@ -11,4 +11,10 @@ public class POINoExisteException extends Exception {
 	public POINoExisteException(String msg){
 		super(msg);
 	}
+
+	@Override
+	public String getMessage() {
+		return "Error: alguno de los POI no existe";
+	}
+	
 }
