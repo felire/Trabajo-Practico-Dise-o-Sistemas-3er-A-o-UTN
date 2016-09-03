866b392fda527cd5f8ac4b3a21f3f80cdab1bcf2
felire <feliress13@gmail.com>
2016-09-03 17:42:05 -0300

    Subo adapter de polygon y point

--- tp-anual/src/main/java/util/Point.java
+++ tp-anual/src/main/java/util/Point.java
@@ -6,15 +6,19 @@ public class Point extends org.uqbar.geodds.Point{
 	private double aY;
 	//private org.uqbar.geodds.Point punto;
 	
+	public double aY(){
+		return this.aY;
+	}
+	
+	public double aX(){
+		return this.aX;
+	}
+	
 	public Point(double aX, double aY) {
 		super(aX, aY);
 		this.aX = aX;
 		this.aY = aY;
 		//punto = new org.uqbar.geodds.Point(aX,aY);
-	}
-	
-	public double distance(Point point){
-		return super.distance(new org.uqbar.geodds.Point(point.aX, point.aY));
 	}
 	
 }
--- tp-anual/src/main/java/util/Polygon.java
+++ tp-anual/src/main/java/util/Polygon.java
@@ -1,5 +1,18 @@
 package util;
 
-public class Polygon {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Polygon extends org.uqbar.geodds.Polygon{
+	
+	List<Point> puntos; 
+	public Polygon(){
+		super();
+		puntos = new ArrayList<Point>();
+	}
+	public void add (Point point){
+		super.add(point);
+		this.puntos.add(point);
+	}
+	
 }
