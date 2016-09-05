6e432c37c98a9b7edcf877054454312aefe9830b
felire <feliress13@gmail.com>
2016-09-05 19:06:30 -0300

    borro persistencia resultado_proceso

--- tp-anual/src/main/java/ar/utn/frba/disenio/tp_anual/model/ResultadoProceso.java
+++ tp-anual/src/main/java/ar/utn/frba/disenio/tp_anual/model/ResultadoProceso.java
@@ -8,16 +8,11 @@ import javax.persistence.*;
 
 import org.uqbarproject.jpa.java8.extras.convert.LocalDateTimeConverter;
 
-@Entity
 public class ResultadoProceso {
-	
-	@Id @GeneratedValue
-	private Integer id;
+
 	
 	Integer cantidadAfectados;
-	@Convert (converter = LocalDateTimeConverter.class)
 	LocalDateTime fechaEjecucion;
-	@Enumerated
 	ProcesoStatus resultadoDelProceso;
 	
 	public ResultadoProceso(Integer cantidadAfectados, LocalDateTime fechaEjecucion, ProcesoStatus resultado){
